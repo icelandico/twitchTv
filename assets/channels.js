6cd17c5a394d51b3278aff9cbf91953994477049
Michal Muszynski <m.muszy@gmail.com>
2019-01-27 10:16:40 +0100

    Using promise all

---
+++ assets/channels.js
@@ -0,0 +1,10 @@
+const channels = [
+  "ESL_SC2",
+  "OgamingSC2",
+  "cretetion",
+  "freecodecamp",
+  "storbeck",
+  "habathcx",
+  "RobotCaleb",
+  "noobs2ninjas",
+];
--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,60 @@
 const apiKey = "mpoy70bghold1xdu9kkittipivqp2p";
-const channelsApi = `https://cors-anywhere.herokuapp.com/wind-bow.glitch.me/twitch-api/channels/freecodecamp`;
-const streamsApi = `https://cors-anywhere.herokuapp.com/wind-bow.glitch.me/twitch-api/streams/freecodecamp`;
-const channels = ["qpHalcy0n","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "RocknightStudios", "lucaelin"];
-const urlsAddress = [channelsApi, streamsApi];
+const channelsApi = `https://cors-anywhere.herokuapp.com/wind-bow.glitch.me/twitch-api/channels/`;
+const streamsApi = `https://cors-anywhere.herokuapp.com/wind-bow.glitch.me/twitch-api/streams/`;
 
-function fetchData(address) {
-  return fetch(address, {
-    headers: {
-      "Client-ID": apiKey,
-      'Access-Control-Allow-Origin': '*'
-    }
-  }).then(res => res.json())
-    .then(data => console.log(data))
-}
+// function fetchData(address) {
+//   return fetch(address, {
+//     headers: {
+//       "Client-ID": apiKey,
+//       'Access-Control-Allow-Origin': '*'
+//     }
+//   }).then(res => res.json())
+//     .then(data => console.log(data))
+// }
 
-//fetchData(streamsApi)
-getData(urlsAddress)
-
-function getData(urls) {
-  Promise.all(urls.map(url =>
-    fetch(url).then(resp => resp.text())
-  )).then(texts => {
-    console.log(texts)
+function fetchData(elem) {
+  let data = {
+    "details": {},
+    "stream": {}
+  };
+  const channelsReq = fetch(channelsApi + elem).then(res => res.json());
+  const streamsReq = fetch(streamsApi + elem).then(res => res.json());
+  const urls = [channelsReq, streamsReq]
+  Promise.all(urls).then(values => {
+    data.channel = values[0]
+    data.stream = values[1]
+    createNode(data)
   })
 }
 
+function getData(channelsList) {
+  channelsList.map(channel => {
+    fetchData(channel);
+  });
+}
+
+getData(channels)
+
+function createNode(data) {
+  const channelInfo = data.channel;
+  const streamingInfo = data.stream;
+//   const channelStructure = `
+//   <li class="channel-container>
+//     <div>
+//       <p>
+//         <a href="${channelInfo.url}">${channelInfo.display_name}</a>
+//       </p>
+//     </div>
+//     <div>
+//       <img src="${channelInfo.logo}" class="channel-logo"
+//     </div>
+//   </li>
+// `;
+  //insertNode(channelStructure)
+}
+
+function insertNode(node) {
+  const channelsList = document.querySelector(".channels-list");
+  channelsList.innerHTML += node;
+}
 
